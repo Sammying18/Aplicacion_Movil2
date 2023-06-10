85cd538e1f4f722079794e970224ac93665c35c1
Sammying18 <104081806+Sammying18@users.noreply.github.com>
2023-06-10 07:47:29 -0500

    Update MainActivity.kt

--- Samantha/Samantha/app/src/main/java/com/example/samantha/MainActivity.kt
+++ Samantha/Samantha/app/src/main/java/com/example/samantha/MainActivity.kt
@@ -3,26 +3,68 @@ package com.example.samantha
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.samantha.ui.theme.SamanthaTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SamanthaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val todoList = remember { mutableStateListOf(TodoItemData("Tarea 1", false), TodoItemData("Tarea 2", false), TodoItemData("Tarea 3", false)) }
+            val newTaskText = remember { mutableStateOf("") }
+
+            MaterialTheme {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    TopAppBar(
+                        title = { Text(text = "Tareas") }
+                    )
+                    TextField(
+                        value = newTaskText.value,
+                        onValueChange = { newTaskText.value = it },
+                        label = { Text(text = "Nueva tarea") },
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Button(
+                        onClick = {
+                            val newTask = newTaskText.value.trim()
+                            if (newTask.isNotEmpty()) {
+                                todoList.add(TodoItemData(newTask, false))
+                                newTaskText.value = ""
+                            }
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "Agregar tarea")
+                    }
+                    LazyColumn(modifier = Modifier.weight(1f)) {
+                        items(todoList) { todo ->
+                            TodoItem(todo = todo) {
+                                todoList.remove(todo)
+                            }
+                        }
+                    }
+                    Button(
+                        onClick = {
+                            todoList.removeAll { it.checked }
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "Eliminar tareas")
+                    }
                 }
             }
         }
@@ -30,17 +72,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TodoItem(todo: TodoItemData, onTaskRemoved: () -> Unit) {
+    var checkedState by remember { mutableStateOf(todo.checked) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SamanthaTheme {
-        Greeting("Android")
+    LaunchedEffect(todo.checked) {
+        checkedState = todo.checked
+    }
+
+    Row(modifier = Modifier.padding(16.dp)) {
+        Checkbox(
+            checked = checkedState,
+            onCheckedChange = { isChecked ->
+                checkedState = isChecked
+                todo.checked = isChecked
+            },
+            modifier = Modifier.padding(end = 16.dp)
+        )
+        Text(text = todo.text)
     }
 }
+
+data class TodoItemData(
+    val text: String,
+    var checked: Boolean
+)
